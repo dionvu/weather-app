f5a714a7226cc946359c6039cec53d36547ba578
Dion Vu <dionvu11@gmail.com>
2024-03-16 18:34:23 -0700

    Seperated files, created input with validation for user.

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,36 @@
+//import Weather from './weather'
+import { getWeather } from './weather'
 import '../src/style/style.css'
 
-interface Weather {
-  location: {
-    name: string;
-    region: string;
-    country: string;
-  };
-  current: {
-    condition: {
-      text: string;
-      icon: string;
+
+const form = document.getElementById('city-form') as HTMLFormElement;
+const input = document.getElementById('city-input') as HTMLInputElement;
+const msg = document.getElementById('input-error') as HTMLSpanElement;
+const img = document.getElementById('img') as HTMLImageElement;
+
+form.addEventListener('submit', async (e) => {
+  if (form.checkValidity()) {
+    e.preventDefault();
+    try {
+      const data = await getWeather(input.value);
+      console.log(data.current.condition.text);
+      img.src = data.current.condition.icon;
+      input.value = '';
+      hideError();
+    }
+    catch {
+      displayError();
+      input.value = '';
     }
   }
+})
+
+function displayError() {
+  msg.style.display = 'block';
+  msg.textContent = 'Invalid city name!';
+};
+
+function hideError() {
+  msg.textContent = '';
+  msg.style.display = 'none';
 }
-
-let city: string | null = '';
-
-city = prompt();
-
-if (city)
-  updateWeather(city);
-
-async function updateWeather(city: string) {
-  try {
-    const data: Weather = await getWeather(city);
-    console.log(data.current.condition.text);
-  }
-  catch (error) {
-    console.log(error);
-  }
-};
-
-async function getWeather(city: string): Promise<Weather> {
-  const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=2b3d697043e1487987335611240703&q=${city}`, { mode: 'cors' });
-
-  if (!response.ok)
-    return Promise.reject(new Error(`ERROR: Unable to fetch data for ${city}.`));
-
-  const data: Weather = await response.json();
-
-  console.table(data);
-
-  return Promise.resolve(data);
-};
---
+++ src/weather.ts
@@ -0,0 +1,37 @@
+export default interface Weather {
+  location: {
+    name: string;
+    region: string;
+    country: string;
+  };
+  current: {
+    condition: {
+      text: string;
+      icon: string;
+    }
+  }
+}
+
+export async function getWeather(city: string): Promise<Weather> {
+  try {
+    const data: Weather = await getApi(city);
+    return data;
+  }
+  catch (error) {
+    console.log(`ERROR: ${error}.`)
+    return Promise.reject(error);
+  }
+};
+
+async function getApi(city: string): Promise<Weather> {
+  const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=2b3d697043e1487987335611240703&q=${city}`, { mode: 'cors' });
+
+  if (!response.ok)
+    return Promise.reject(new Error(`ERROR: Unable to fetch data for city: ${city}.`));
+
+  const data: Weather = await response.json();
+
+  console.table(data);
+
+  return Promise.resolve(data);
+};
