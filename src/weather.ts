38debc25bbbddf9f39563fee6087d3afdff016ec
Dion Vu <dionvu11@gmail.com>
2024-07-06 12:56:42 -0700

    rando changes.

--- src/weather.ts
+++ src/weather.ts
@@ -6,32 +6,34 @@ export default interface Weather {
     region: string;
     country: string;
     localtime: string;
-  },
+  };
   current: {
     condition: {
       text: string;
       icon: string;
-    }
+    };
     temp_f: number;
-  },
+  };
   forecast: {
     forecastday: {
       hour: {
         time: string;
         temp_f: number;
-      }[]
-    }[]
-  },
-};
+      }[];
+    }[];
+  };
+}
 
-const city = document.getElementById('city') as HTMLSpanElement;
-const location = document.getElementById('location') as HTMLSpanElement;
-const condition = document.getElementById('condition') as HTMLSpanElement;
-const tempurature = document.getElementById('temp') as HTMLElement;
-const locationImage = document.getElementById('location-image') as HTMLImageElement;
-const localTime = document.getElementById('localtime') as HTMLSpanElement;
-const search = document.getElementById('city-search') as HTMLInputElement;
-const weatherContainer = document.getElementById('weather') as HTMLElement;
+const city = document.getElementById("city") as HTMLSpanElement;
+const location = document.getElementById("location") as HTMLSpanElement;
+const condition = document.getElementById("condition") as HTMLSpanElement;
+const tempurature = document.getElementById("temp") as HTMLElement;
+const locationImage = document.getElementById(
+  "location-image",
+) as HTMLImageElement;
+const localTime = document.getElementById("localtime") as HTMLSpanElement;
+const search = document.getElementById("city-search") as HTMLInputElement;
+const weatherContainer = document.getElementById("weather") as HTMLElement;
 
 /**
  * @brief Fetches weather data for given city.
@@ -39,22 +41,26 @@ const weatherContainer = document.getElementById('weather') as HTMLElement;
  * @returns Promise<Weather>
  */
 export async function getWeather(city: string): Promise<Weather> {
-
   try {
-    const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=2b3d697043e1487987335611240703&q=${city}`, { mode: 'cors' });
+    // lol
+    const response = await fetch(
+      `https://api.weatherapi.com/v1/forecast.json?key=2b3d697043e1487987335611240703&q=${city}`,
+      { mode: "cors" },
+    );
 
     if (!response.ok)
-      return Promise.reject(new Error(`ERROR: Unable to fetch data for city: ${city}.`));
+      return Promise.reject(
+        new Error(`ERROR: Unable to fetch data for city: ${city}.`),
+      );
 
     const data: Weather = await response.json();
 
     return Promise.resolve(data);
-  }
-  catch (error) {
-    console.log(`ERROR: ${error}.`)
+  } catch (error) {
+    console.log(`ERROR: ${error}.`);
     return Promise.reject(error);
   }
-};
+}
 
 /**
  * @brief Updates the weather at current time (forecast handled seperately).
@@ -69,18 +75,16 @@ export async function updateWeather(cityName: string): Promise<void> {
       location.textContent = data.location.country;
     else if (data.location.region.length <= data.location.country.length)
       location.textContent = data.location.region;
-    else
-      location.textContent = data.location.country;
+    else location.textContent = data.location.country;
 
     locationImage.src = url;
     city.textContent = data.location.name;
     condition.textContent = data.current.condition.text;
     localTime.textContent = data.location.localtime;
-    tempurature.textContent = data.current.temp_f.toString() + 'F';
-    search.value = '';
-  }
-  catch (error) {
-    weatherContainer.style.display = 'flex';
+    tempurature.textContent = data.current.temp_f.toString() + "F";
+    search.value = "";
+  } catch (error) {
+    weatherContainer.style.display = "flex";
     return Promise.reject(`ERROR: ${error}`);
   }
-};
+}
